87df665d141145024e410bbdbee512470f5e3ead
jamesMuriithi <jamesmuriithi777@gmail.com>
2026-05-12 18:41:35 +0300

    Refactor geolocation tracker to save and open map in browser

--- geolocation_tracker.py
+++ geolocation_tracker.py
@@ -1,5 +1,4 @@
-import requests
-import folium
+import requests, folium, webbrowser, os
 
 def get_geolocation(ip=''):
     url = f"http://ip-api.com/json/{ip}"
@@ -31,14 +30,21 @@ def create_map(data):
         tooltip="Click for details"
     ).add_to(my_map)
     
+    # Save the map as an .html file
+    file_name = "location_map.html"
+    my_map.save(file_name)
     
     print(f"\nSuccess! Location found: {location_name}")
     print(f"Coordinates: {lat}, {lon}")
     print(f"Timezone: {data['timezone']}")
+    print(f"Map saved as {file_name}")
+    
+    # Automatically open the map in your browser
+    webbrowser.open('file://' + os.path.realpath(file_name))
 
 if __name__ == "__main__":
     print("--- GEOLOCATION TRACKER --- \n")
-    target_ip = input("Enter IP address(eg,'8.8.8.8') to track (or press Enter for your own): ")
+    target_ip = input("Enter IP address(eg,'8.8.8.8') to track (or press Enter for your own): \n")
     
     geo_data = get_geolocation(target_ip)
     
